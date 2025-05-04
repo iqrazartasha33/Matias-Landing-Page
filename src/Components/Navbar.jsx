00b30051c56d8f24b1f5b338ea6f30ffa5a7b2e0
iqrazartasha33 <iqrazartasha33@gmail.com>
2025-05-04 23:55:46 +0500

    Initial commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,44 +5,43 @@ function Navbar() {
     menu.classList.toggle('show');
   }
 
-    return (
-      <div className="main">
-        <h1 className="heading"><span>M</span>Matias</h1>
-        
-        <div className="hamburger" onClick={toggleMenu}>☰</div>
+  return (
+    <div className="main">
+      <h1 className="heading"><span>M</span>Matias</h1>
 
-        <div>
-          <ul className="list"> 
-            <li>
-              <a href="" className="active">Home</a>
-            </li>
-            <li>
-              <a href="">About</a>
-            </li>
-            <li>
-              <a href="">Works</a>
-            </li>
-            <li>
-              <a href="">Services</a>
-            </li>
-            <li>
-              <a href="">Testimonial</a>
-            </li>
-            <li>
-              <a href="">Blog</a>
-            </li>
-            <li>
-              <a href="">Contact</a>
-            </li>
-          </ul>
-          
-        </div>
-        <div >
+      <div className="hamburger" onClick={toggleMenu}>☰</div>
+
+      <div>
+        <ul className="list"> 
+          <li>
+            <a href="#home" className="active">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#works">Works</a>
+          </li>
+          <li>
+            <a href="#services">Services</a>
+          </li>
+          <li>
+            <a href="#testimonial">Testimonial</a>
+          </li>
+          <li>
+            <a href="#blog">Blog</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+
+      <div>
         <Button />
       </div>
-      </div>
-    );
-  }
-  
-  export default Navbar;
-  
+    </div>
+  );
+}
+
+export default Navbar;
